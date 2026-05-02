00658a823bf47b793bf03596edc08eae65875102
Jimmy Hou <bearhedge@Jimmys-MacBook-Pro.local>
2026-05-02 20:17:38 +0800

    Update apeyolo to v0.0.2

--- Formula/apeyolo.rb
+++ Formula/apeyolo.rb
@@ -1,24 +1,24 @@
 class Apeyolo < Formula
   desc "AI trading assistant for 0DTE SPY options"
   homepage "https://apeyolo.com"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://storage.googleapis.com/apeyolo-cli-releases/v0.0.1/apeyolo-darwin-arm64"
-      sha256 "2b56f06545a3369e70fdff9716a975e88a5a7a4f0383de918beb23c1089e7ebf"
+      url "https://storage.googleapis.com/apeyolo-cli-releases/v0.0.2/apeyolo-darwin-arm64"
+      sha256 "6d18a6b00ffe32949f5fd8fde5940c2093f85f43cd66a352132c0baf2978006d"
     end
     on_intel do
-      url "https://storage.googleapis.com/apeyolo-cli-releases/v0.0.1/apeyolo-darwin-x64"
-      sha256 "3adc0649278658b5cb314db0c8ed93b8df625231c0361b1d0b1b2fbd1fbb74da"
+      url "https://storage.googleapis.com/apeyolo-cli-releases/v0.0.2/apeyolo-darwin-x64"
+      sha256 "2ac114f7ee99a2f99949f5a2c4fcc935881d8d1754381c106ccd3ffc0d4ac6f7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://storage.googleapis.com/apeyolo-cli-releases/v0.0.1/apeyolo-linux-x64"
-      sha256 "a5bd67373396053b80f2ecf053b1f92fbd7fe17b65812967d2302f81c2c0699d"
+      url "https://storage.googleapis.com/apeyolo-cli-releases/v0.0.2/apeyolo-linux-x64"
+      sha256 "0eb6ad7bcf62ad7925962383f5c03397b85947afeb73b169609dd3e4b2c1745b"
     end
   end
 
